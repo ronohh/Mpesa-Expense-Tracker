f270e25aba7a794d6f47992863d6b2d30c79c346
Ronohh <cimanu88@gmail.com>
2026-08-17 16:39:42 +0300

    project setup with MERN

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config"
+
+const app = express();
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
