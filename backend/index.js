72952ee7c39b19ff4f63fe04cbe7d791d9722246
Ronohh <cimanu88@gmail.com>
2026-08-18 12:11:11 +0300

    connection to mongo DB atlas

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config"
+import connectDB from "./config/db_connection.js";
 
 const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//DB
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
