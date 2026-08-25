fe9e19bf3c18bbc41b2c69baf867f14e479111a3
Ronohh <cimanu88@gmail.com>
2026-08-25 17:26:01 +0300

    login styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,20 +4,20 @@ const Login = () => {
     
 
     return(
-        <div>
-            <div>
-                <h1>Login</h1>
+        <div className="flex flex-col items-center justify-center ">
+            <div className="border shadow-lg p-6 w-80 bg-white">
+                <h1 className="text-2xl font-bold mb-4">Login</h1>
                 <form >
-                    <div>
-                        <label>Username</label>
-                        <input type="text"/>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Username</label>
+                        <input type="text" className="w-full px-3 py-2 border rounded"/>
                     </div>
-                    <div>
+                    <div className="mb-4">
                         <label>Password</label>
-                        <input type="text" />
+                        <input type="text" className=" border w-full px-3 py-2 rounded"/>
                     </div>
                     <div>
-                        <button type="submit" >Login</button>
+                        <button type="submit" className="w-full bg-green-600 text-white py-2 rounded">Login</button>
                     </div>
                 </form>
             </div>
