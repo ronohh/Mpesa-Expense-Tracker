0a3920a2a52472300fe4fcc5a778993d6a1815b1
Ronohh <cimanu88@gmail.com>
2026-09-07 20:11:59 +0300

    Transation Model

---
+++ backend/models/TransactionModel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+    description: {type: String, required: true},
+    amount: {type: Number, required: true},
+    category: {type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    date: { type: Date, required: true }
+}, {timestamp: true});
+
+const Transaction = mongoose.models.Transaction || mongoose.model('Transaction', transactionSchema);
+
+export default Transaction;
