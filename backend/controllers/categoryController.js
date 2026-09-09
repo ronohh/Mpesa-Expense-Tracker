f905e884d791e9b88e0e0806113e3c9b8c56db9f
Ronohh <cimanu88@gmail.com>
2026-09-09 20:03:50 +0300

    category controller

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,22 @@
+import Category from "../models/Category.js";
+
+const addCategory = async (req,res) => {
+    try {
+        const { name, type, icon } = req.body;
+
+        if( !name || !type) {
+            return res.status(400).json({ message: "Name and type are required"});
+        }
+
+        const existingCategory = await Category.findOne({ name,type,user: req.user._id});
+        if(existingCategory) {
+            return res.status(400).json({ message: "Category already exists"});
+         }
+
+         const category = await Category.create({name,type,icon,user: req.user._id });
+         res.status(201).json({ message: "Category added successfully", category });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -6,14 +6,16 @@ const Categories = () => {
             <h1>Categories</h1>
 
             <form className="bg-white shadow rounded-lg p-6 mb-6">
-                <h2 bg-black>Add Category</h2>
-                <input type="text" placeholder="Enter category name" />
-                <select>
-                    <option value="expense">Expense</option>
-                    <option value="income">Income</option>
-                </select>
-                <input type="submit" value="Add Category" />
-                <button>Add Category</button>
+                <h2 className="bg-black text-white p-2">Add Category</h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <input type="text" placeholder="Enter category name" />
+                    <select>
+                        <option value="expense">Expense</option>
+                        <option value="income">Income</option>
+                    </select>
+                    <input type="submit" value="Add Category" />
+                    <button>Add Category</button>
+                </div>
             </form>
         </div>
     );
