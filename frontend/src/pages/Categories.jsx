1d641186a234162299fe3d0b8a2dbb5f887d236d
Ronohh <cimanu88@gmail.com>
2026-09-21 20:10:36 +0300

    handle change function

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -26,6 +26,14 @@ const fetchCategories = async () => {
         })
 }
 
+const handleChange = (e) => {
+    const { name, value} = e.target;
+    setFormData((prev) => ({
+        ...prev,
+        [name]: value
+    }));
+}
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -54,12 +62,12 @@ const fetchCategories = async () => {
                 <h2 className="bg-green-500 text-black font-bold p-2">Add Category</h2>
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
-                    <input type="text" placeholder="Enter category name" className="border rounded px-4 py-2" required />
-                    <select className="border rounded px-4 py-2">
+                    <input type="text" name="name" placeholder="Enter category name" className="border rounded px-4 py-2" required onChange={handleChange} />
+                    <select name="type" className="border rounded px-4 py-2" onChange={handleChange}>
                         <option value="expense">Expense</option>
                         <option value="income">Income</option>
                     </select>
-                    <input type="text" placeholder="Icon name" className="border rounded px-4 py-2"/>
+                    <input type="text" name="icon" placeholder="Icon name" className="border rounded px-4 py-2" onChange={handleChange}/>
                     <input type="submit" value="Add Category" />
                     <button type="submit" className="mt-4 bg-green-500 text-white px-6 py-2 rounded hover-green-700">Add Category</button>
                 </div>
